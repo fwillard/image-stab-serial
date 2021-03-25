0f4c90d6ffaead89b0e9cad5934e5b6faa40bdb5
Finn Willard <finn.willard@gmail.com>
2021-03-25 10:58:32 -0700

    Test OpenCV by displaying image

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
+#include <opencv2/opencv.hpp>
+
 #include <iostream>
 
 int main(int argc, char **argv) {
+  cv::Mat img = cv::imread("./../test_img.jpg");
 
-  std::cout << "Hello world!" << std::endl;
+  if (img.empty()) {
+    std::cout << "Could not open or find the image" << std::endl;
+    std::cin.get(); // wait for any key press
+    return -1;
+  }
 
+  cv::String windowName = "Open CV Test"; // Name of the window
+
+  cv::namedWindow(windowName); // Create a window
+
+  cv::imshow(windowName, img); // Show our image inside the created window.
+
+  cv::waitKey(0); // Wait for any keystroke in the window
+
+  cv::destroyWindow(windowName); // destroy the created window
   return 0;
 }
