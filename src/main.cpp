112ce6a55ca0f22a9eb08719cb575383394672fa
Finn Willard <finn.willard@gmail.com>
2021-03-25 12:55:20 -0700

    Basic LK Optical Flow tracking

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <opencv2/video.hpp>
 #include <opencv2/videoio.hpp>
 
+static void help(){
+    std::cout << "usage: ImageStab video_file" << std::endl;
+}
 int main(int argc, char **argv) {
     // parser keys
     std::string keys =  "{help h usage ? |      | print this message     }"
@@ -15,7 +18,7 @@ int main(int argc, char **argv) {
     
     // if parser contains help flag, display usage then exit
     if (parser.has("help")) {
-        parser.printMessage();
+        help();
         return 0;
     }
     
@@ -28,10 +31,85 @@ int main(int argc, char **argv) {
     
     cv::VideoCapture capture(filename);
     if (!capture.isOpened()) {
-        // error in opening the video input
+        // error in opening the video
         std::cerr << "Unable to open file!" << std::endl;
         return 0;
     }
+
     
+    std::vector<cv::Scalar> colors;
+    cv::RNG rng;
+    for (auto i = 0; i < 100; i++) {
+        auto r = rng.uniform(0, 256);
+        auto g = rng.uniform(0, 256);
+        auto b = rng.uniform(0, 256);
+        
+        colors.push_back(cv::Scalar(r,g,b));
+    }
+    
+    cv::Mat old_frame, old_gray;
+    std::vector<cv::Point2f> p0, p1;
+
+    // Read first frame and find corners in it
+    capture >> old_frame;
+    cvtColor(old_frame, old_gray, cv::COLOR_BGR2GRAY);
+    goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, cv::Mat(), 7, false, 0.04);
+
+    // Create a mask image for drawing purposes
+    cv::Mat mask = cv::Mat::zeros(old_frame.size(), old_frame.type());
+    
+    bool save = false;
+    int counter = 0;
+    while(true){
+        //read new frame
+        cv::Mat frame, frame_gray;
+        capture >> frame;
+        
+        if(frame.empty()){
+            break;;
+        }
+        
+        cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+        
+        std::vector<uchar> status;
+        std::vector<float> err;
+        //define our termination criteria for the LK algorithm
+        //here we are using iteration count + epsilon value
+        //count = 10, eps = 0.03
+        cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 10, 0.03);
+        
+        //calculate optical flow
+        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, cv::Size(15, 15), 2, criteria);
+        
+        std::vector<cv::Point2f> good_new;
+        // Visualization part
+        for(uint i = 0; i < p0.size(); i++)
+        {
+            // Select good points
+            if(status[i] == 1) {
+                good_new.push_back(p1[i]);
+                // Draw the tracks
+                line(mask,p1[i], p0[i], colors[i], 2);
+                circle(frame, p1[i], 5, colors[i], -1);
+            }
+        }
+
+        // Display the demo
+        cv::Mat img;
+        add(frame, mask, img);
+        if (save) {
+            std::string save_path = "./optical_flow_frames/frame_" + std::to_string(counter) + ".jpg";
+            imwrite(save_path, img);
+        }
+        imshow("flow", img);
+        int keyboard = cv::waitKey(25);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+
+        // Update the previous frame and previous points
+        old_gray = frame_gray.clone();
+        p0 = good_new;
+        counter++;
+    }
     return 0;
 }
