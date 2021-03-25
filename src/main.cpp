b3ebe4860c80b0e6608fc9855368a8ce0c9a1095
Finn Willard <finn.willard@gmail.com>
2021-03-25 11:58:26 -0700

    command line parser code to read in video file

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,37 @@
-#include <opencv2/opencv.hpp>
-
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
 
 int main(int argc, char **argv) {
-  cv::Mat img = cv::imread("./../test_img.jpg");
-
-  if (img.empty()) {
-    std::cout << "Could not open or find the image" << std::endl;
-    std::cin.get(); // wait for any key press
-    return -1;
-  }
-
-  cv::String windowName = "Open CV Test"; // Name of the window
-
-  cv::namedWindow(windowName); // Create a window
-
-  cv::imshow(windowName, img); // Show our image inside the created window.
-
-  cv::waitKey(0); // Wait for any keystroke in the window
-
-  cv::destroyWindow(windowName); // destroy the created window
-  return 0;
+    // parser keys
+    std::string keys =  "{help h usage ? |      | print this message     }"
+                        "{@video         |      | video to be stabilized }";
+    
+    cv::CommandLineParser parser(argc, argv, keys);
+    parser.about("Serial Image Stabilization v1.0");
+    
+    // if parser contains help flag, display usage then exit
+    if (parser.has("help")) {
+        parser.printMessage();
+        return 0;
+    }
+    
+    // get the video file name from the parser
+    std::string filename = cv::samples::findFile(parser.get<std::string>("@video"));
+    if (!parser.check()) {
+        parser.printErrors();
+        return 0;
+    }
+    
+    cv::VideoCapture capture(filename);
+    if (!capture.isOpened()) {
+        // error in opening the video input
+        std::cerr << "Unable to open file!" << std::endl;
+        return 0;
+    }
+    
+    return 0;
 }
